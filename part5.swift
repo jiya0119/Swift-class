211c0ec6611b96be0ec12d334d2235c1926cec94
jiya0119 <jiyoonk119@gmail.com>
2018-01-22 00:43:34 +0900

    closure축약
    축약까지 함

--- part5.swift
+++ part5.swift
@@ -47,3 +47,60 @@ let transaction032701 = 120.7 //현재 가격
 additional = addVAT
 
 let price032701 = additional(transaction032701) //최종가격 132.77
+
+
+
+//함수타입을 다른 함수의 매개변수로 사용하기
+
+func finalPrice(source:Double, additional:(Double) -> Double) -> Double {
+//additional이라는 매개변수로 Double을 받아서 Double을 return하는 함수를 받음
+//최종적으로 Double을 return함.
+  let price = additional(source)
+  //매개변수로 넘어온 additional이라는 함수에 source값을 넣어서 계산
+  //그 계산 결과를 return
+  return price
+}
+
+let price032702 = finalPrice(350.0, additional: couponDiscount)
+//couponDiscount이라는 함수를 매개변수로 넘김.
+//결과:315
+
+
+
+//closure
+//1등시민으로 사용가능한 독립적인 코드조각 (매개변수, return type, 할당 가능)
+//자신이 정의된 환경을 캡쳐해서 저장한 뒤, (문을) 닫아버린다.
+
+//closure의 문법
+//블록으로 감싸있다
+{(params) -> returnType in //in이 나오면 클로저인 것!!!!!!!!
+  statements //매개변수를 이용해서 returnType을 만들어내는 구문
+}
+
+//클로저는 생략, 축약이 가능, in이 생략되기도.
+//가능한 짧게 만들어야 함.
+
+//param은 이름이 아니라 위치로 참조해서 생략
+//returnType은 명백한 경우 생략
+//addVAT와 couponDiscount를 클로저로 만들어보자.
+
+let addVATClosure = { (source: Double) -> Double in
+  return source * 1.1
+}
+
+let addVATClosure2 = { source in //type과 returnType생략
+  return source * 1.1
+}
+
+let addVATClosure3 = { source in
+  source * 1.1 //return도 생략
+}
+
+let addVATClosure4 = { $0* 1.1}
+
+let couponDiscountClosure = {(source: Double) -> Double in
+  return source * 0.9
+}
+
+let couponDiscountClosure2 = {$0 * 0.9}
+let price032703 = addVATClosure(157.6) //173.36
