f4956e7d17ea670d362028a65aa280a221f59329
jiya0119 <jiyoonk119@gmail.com>
2018-01-21 21:24:09 +0900

    Init end
    end of init

--- enum.swift
+++ enum.swift
@@ -48,9 +48,21 @@ class Employee {
   var name: String?
   var phoneNumber: String?
   var boss: Employee?
+
+  init (name: String) {
+    self.name = name
+  }
+  convenience init (name: String, phone: String) {
+    self.init(name:name)
+    self.phoneNumber = phone
+
+  }
+  //이것이 class의 초기화가 구조체같은 value type의 초기화보다 복잡한 면.
+  //여러개의 initializer를 만들어 놓고 편리한 것을 쓰면 됨
+
 }
-let me: Employee = Employee()
-me.name = "Alex"
+let me: Employee = Employee(name: "Alex")
+// me.name = "Alex"
 me.phoneNumber = "010-1234-5678"
 //...
 /*
@@ -99,6 +111,18 @@ enum TaskType {
     }
   }
 }
+
+init (type: TaskType, owner: Employee) { // Task를 초기화하는 데 있어 필수적인 값
+  self.type = type //매개변수
+  self.title = type.typeTitle //얻어와서 초기화
+  self.owner = owner //매개변수
+  self.time = nil //옵셔널
+  self.participant = nil
+}
+
+//초기화 메소드를 만들었기 때문에 Task 인스턴스를 좀 더 간단하게 만들 수 있음
+
+
 }
 class Employee {
     var name: String
@@ -110,4 +134,19 @@ class Employee {
 //그때그때 TaskType의 값에 따라서 특정한 값들을 제공
 // 이외의 title, time, owner, participant는 어떤 메모리 영역을 가지고 있는 property
 //즉 stored property임
-//우리가 만든 Task 구조체와 employee 구조체는 아직 초기화 메소드를
+//우리가 만든 Task 구조체와 employee 구조체는 아직 초기화 메소드를 갖고 있지 않음
+//그래서 callTask를 만들 때 Task 안에 있는 stored property들을 그때그때 초기화해야만 했음
+//좀 더 간편하게 초기화할 수 있도록 Task 안에 init 메소드를 구현해자
+
+
+
+
+
+
+
+
+//method
+//어떤 타입에 종속된 함수
+//그 타입으로 만든 인스턴스에서 필요한 작업이나 기능을 함수로 추상화해서 만들어 넣은 것
+//class, structure, enumeration은 모두 인스턴스 메소드를 가질 수 있다
+//self는 만들어진 인스턴스 그 자신을 가리킴
